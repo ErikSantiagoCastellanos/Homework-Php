ec6106c837430591bde06e5648a1e09c328ddab6
ErikSantiagoCastellanos <chilamarae@gmail.com>
2025-05-13 17:39:56 -0500

    Index

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Catalogo</title>
+</head>
+<body>
+<h1>Catalogo</h1>
+<a href="" > Ver carrito 0 </a>
+<div class = "contenedor-productos">
+
+    <div class = "producto">
+        <h2>Producto 1</h2>
+        <p>Descripción del producto 1</p>
+        <p>Precio: $0</p>
+        <button class="btn btn-primary">Agregar al carrito</button>
+		<br>
+        <button class="btn btn-secondary">Productos disponibles</button>
+    </div>
+    <div class = "producto">
+        <h2>Producto 2</h2>
+        <p>Descripción del producto 2</p>
+        <p>Precio: $00</p>
+        <button class="btn btn-primary">Agregar al carrito</button> 
+		<br>
+        <button class="btn btn-secondary">Productos disponibles</button> 
+</body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</html>
